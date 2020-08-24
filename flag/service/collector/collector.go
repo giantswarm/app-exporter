3d6030b1c70ffefaa394b47f190b3b65f6613652
Jihyuk Bok <jhbok@giantswarm.io>
2020-08-24 12:53:20 +0200

    adding flag

---
+++ flag/service/collector/collector.go
@@ -0,0 +1,7 @@
+package collector
+
+import "github.com/giantswarm/app-collector/flag/service/collector/apps"
+
+type Collector struct {
+	Apps apps.Apps
+}
--- flag/service/service.go
+++ flag/service/service.go
@@ -1,10 +1,12 @@
 package service
 
 import (
+	"github.com/giantswarm/app-collector/flag/service/collector"
 	"github.com/giantswarm/operatorkit/v2/pkg/flag/service/kubernetes"
 )
 
 // Service is an intermediate data structure for command line configuration flags.
 type Service struct {
+	Collector  collector.Collector
 	Kubernetes kubernetes.Kubernetes
 }
