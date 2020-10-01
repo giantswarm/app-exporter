7fdd50c1edb0f4cb8ab6ee3ef50c33fb819c5fd6
GitHub Action <action@github.com>
2020-10-01 09:07:06 +0000

    Bump version to 0.2.2-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -9,7 +9,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "app-exporter"
 	source      = "https://github.com/giantswarm/app-exporter"
-	version     = "0.2.1"
+	version     = "0.2.2-dev"
 )
 
 // AppTenantVersion is always 1.0.0 for tenant cluster app CRs using Helm 2.
