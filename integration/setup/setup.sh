77f6bef0b4cec83ece2ad03eb812b365ee82f18b
Ross Fairbanks <ross@rossfairbanks.com>
2020-10-06 15:55:35 +0200

    Download binary release

--- integration/setup/setup.sh
+++ integration/setup/setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-go get github.com/giantswarm/apptestctl@v0.1.0
-apptestctl bootstrap --kubeconfig="$(kind get kubeconfig)"
+curl -L https://github.com/giantswarm/apptestctl/releases/download/v0.2.0/apptestctl-v0.2.0-linux-amd64.tar.gz > ./apptestctl.tar.gz
+tar xzvf apptestctl.tar.gz
+chmod u+x apptestctl-v0.2.0-darwin-amd64/apptestctl
+mv apptestctl-v0.2.0-darwin-amd64/apptestctl .
+
+./apptestctl bootstrap --kubeconfig="$(kind get kubeconfig)"
