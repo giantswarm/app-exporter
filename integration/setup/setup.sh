aec6d43ee2b26e0befc21a7944fbec0b14cd69d0
Ross Fairbanks <ross@rossfairbanks.com>
2020-10-06 16:16:07 +0200

    Fix script

--- integration/setup/setup.sh
+++ integration/setup/setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-curl -L https://github.com/giantswarm/apptestctl/releases/download/v0.2.0/apptestctl-v0.2.0-linux-amd64.tar.gz > ./apptestctl.tar.gz
+curl -L https://github.com/giantswarm/apptestctl/releases/download/v0.2.0/apptestctl-v0.2.0-linux-amd64.tar.gz > /tmp/apptestctl.tar.gz
+cd /tmp
 tar xzvf apptestctl.tar.gz
-chmod u+x apptestctl-v0.2.0-darwin-amd64/apptestctl
-mv apptestctl-v0.2.0-darwin-amd64/apptestctl .
+chmod u+x /tmp/apptestctl-v0.2.0-linux-amd64/apptestctl
+mv /tmp/apptestctl-v0.2.0-linux-amd64/apptestctl /usr/local/bin
 
-./apptestctl bootstrap --kubeconfig="$(kind get kubeconfig)"
+apptestctl bootstrap --kubeconfig="$(kind get kubeconfig)"
