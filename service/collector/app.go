b6448700b496c72c425175d8d98551cf3d9d3a6a
Ross Fairbanks <ross@rossfairbanks.com>
2021-02-19 15:10:15 +0100

    Revert "Fix log"
    This reverts commit ae45d3acece2cb121811daa080c88a99eb6f104f.

--- service/collector/app.go
+++ service/collector/app.go
@@ -6,11 +6,13 @@ import (
 	"strings"
 	"time"
 
+	"github.com/giantswarm/apiextensions/v3/pkg/apis/application/v1alpha1"
 	"github.com/giantswarm/app/v4/pkg/key"
 	"github.com/giantswarm/k8sclient/v5/pkg/k8sclient"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
 	"github.com/prometheus/client_golang/prometheus"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -45,8 +47,7 @@ type AppConfig struct {
 	K8sClient k8sclient.Interface
 	Logger    micrologger.Logger
 
-	AppTeamMapping map[string]string
-	DefaultTeam    string
+	DefaultTeam string
 }
 
 // App is the main struct for this collector.
@@ -54,8 +55,7 @@ type App struct {
 	k8sClient k8sclient.Interface
 	logger    micrologger.Logger
 
-	appTeamMapping map[string]string
-	defaultTeam    string
+	defaultTeam string
 }
 
 // NewApp creates a new App metrics collector
@@ -67,9 +67,6 @@ func NewApp(config AppConfig) (*App, error) {
 		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
 	}
 
-	if config.AppTeamMapping == nil {
-		return nil, microerror.Maskf(invalidConfigError, "%T.AppTeamMapping must not be empty", config)
-	}
 	if config.DefaultTeam == "" {
 		return nil, microerror.Maskf(invalidConfigError, "%T.DefaultTeam must not be empty", config)
 	}
@@ -78,8 +75,7 @@ func NewApp(config AppConfig) (*App, error) {
 		k8sClient: config.K8sClient,
 		logger:    config.Logger,
 
-		appTeamMapping: config.AppTeamMapping,
-		defaultTeam:    config.DefaultTeam,
+		defaultTeam: config.DefaultTeam,
 	}
 
 	return c, nil
@@ -111,9 +107,10 @@ func (c *App) collectAppStatus(ctx context.Context, ch chan<- prometheus.Metric)
 	}
 
 	for _, app := range r.Items {
-		team, ok := c.appTeamMapping[key.AppName(app)]
-		if !ok {
-			team = c.defaultTeam
+		team, err := c.getTeam(ctx, app)
+		if err != nil {
+			c.logger.Log("level", "warning", "message", fmt.Sprintf("could not convert cordon-until for app %q", key.AppName(app)), "stack", fmt.Sprintf("%#v", err))
+			continue
 		}
 
 		ch <- prometheus.MustNewConstMetric(
@@ -135,7 +132,7 @@ func (c *App) collectAppStatus(ctx context.Context, ch chan<- prometheus.Metric)
 
 		t, err := convertToTime(key.CordonUntil(app))
 		if err != nil {
-			c.logger.Log("level", "warning", "message", fmt.Sprintf("could not get team for app %q", key.AppName(app)), "stack", fmt.Sprintf("%#v", err))
+			c.logger.Log("level", "warning", "message", fmt.Sprintf("could not convert cordon-until for app %q", key.AppName(app)), "stack", fmt.Sprintf("%#v", err))
 			continue
 		}
 
@@ -148,6 +145,24 @@ func (c *App) collectAppStatus(ctx context.Context, ch chan<- prometheus.Metric)
 		)
 	}
 	return nil
+}
+
+func (c *App) getTeam(ctx context.Context, app v1alpha1.App) (string, error) {
+	name := key.AppCatalogEntryName(app.Spec.Catalog, app.Name, app.Spec.Version)
+
+	ace, err := c.k8sClient.G8sClient().ApplicationV1alpha1().AppCatalogEntries(metav1.NamespaceDefault).Get(ctx, name, metav1.GetOptions{})
+	if apierrors.IsNotFound(err) {
+		return c.defaultTeam, nil
+	} else if err != nil {
+		return "", microerror.Mask(err)
+	}
+
+	team := key.AppCatalogEntryTeam(*ace)
+	if team == "" {
+		team = c.defaultTeam
+	}
+
+	return team, nil
 }
 
 func convertToTime(input string) (time.Time, error) {
