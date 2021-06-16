462dc41691aef2747891e25be3e531ea131ba1fa
Jihyuk Bok <jhbok@giantswarm.io>
2021-06-16 14:02:38 +0200

    fix a test

--- integration/test/metrics/metrics_test.go
+++ integration/test/metrics/metrics_test.go
@@ -93,9 +93,10 @@ func TestMetrics(t *testing.T) {
 			t.Fatalf("expected nil got %#q", err)
 		}
 
-		expectedAppMetric := fmt.Sprintf("app_operator_app_info{app=\"%s\",catalog=\"%s\",name=\"%s\",namespace=\"%s\",status=\"%s\",team=\"batman\",version=\"%s\"} 1",
+		expectedAppMetric := fmt.Sprintf("app_operator_app_info{app=\"%s\",catalog=\"%s\",desired_version=\"%s\",name=\"%s\",namespace=\"%s\",status=\"%s\",team=\"batman\",version=\"%s\"} 1",
 			app.Spec.Name,
 			app.Spec.Catalog,
+			app.Status.Version,
 			app.Name,
 			app.Namespace,
 			app.Status.Release.Status,
