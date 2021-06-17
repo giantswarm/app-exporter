9b882e4406a78f54c66959ee7e896a712af18fef
GitHub Action <action@github.com>
2021-06-17 16:03:06 +0000

    Bump version to 0.5.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -9,7 +9,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "app-exporter"
 	source      = "https://github.com/giantswarm/app-exporter"
-	version     = "0.5.0"
+	version     = "0.5.1-dev"
 )
 
 func Description() string {
