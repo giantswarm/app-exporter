7b85b060c73b4f26bc7878f55c5a4c6e6cee1e9d
taylorbot <dev@giantswarm.io>
2023-04-25 08:02:03 +0000

    Release v0.17.5

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -9,7 +9,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "app-exporter"
 	source      = "https://github.com/giantswarm/app-exporter"
-	version     = "0.17.5-dev"
+	version     = "0.17.5"
 )
 
 func Description() string {
