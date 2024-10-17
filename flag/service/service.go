2ccb6267638e802ea1107e71cc7d36161b92f966
Joe Salisbury <salisbury.joseph@gmail.com>
2024-10-17 09:18:38 +0100

    Removes operatorkit dependency (#493)

--- flag/service/service.go
+++ flag/service/service.go
@@ -1,13 +1,36 @@
 package service
 
 import (
-	"github.com/giantswarm/operatorkit/v7/pkg/flag/service/kubernetes"
-
 	"github.com/giantswarm/app-exporter/flag/service/collector"
 )
+
+// TLS is a data structure for Kubernetes TLS configuration with command line
+// flags.
+type TLS struct {
+	CAFile  string
+	CrtFile string
+	KeyFile string
+}
+
+// Watch is a data structure to hold Kubernetes specific configuration
+// for watching for Kubernetes resources.
+type Watch struct {
+	Namespace string
+}
+
+// Kubernetes is a data structure to hold Kubernetes specific command line
+// configuration flags.
+type Kubernetes struct {
+	Address        string
+	InCluster      string
+	KubeConfig     string
+	KubeConfigPath string
+	TLS            TLS
+	Watch          Watch
+}
 
 // Service is an intermediate data structure for command line configuration flags.
 type Service struct {
 	Collector  collector.Collector
-	Kubernetes kubernetes.Kubernetes
+	Kubernetes Kubernetes
 }
