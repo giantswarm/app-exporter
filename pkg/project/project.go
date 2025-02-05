9671c200b25cc2b558125df59639e3ce6430b5bf
taylorbot <dev@giantswarm.io>
2025-02-05 14:44:40 +0000

    Bump version to 1.0.2-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -9,7 +9,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "app-exporter"
 	source      = "https://github.com/giantswarm/app-exporter"
-	version     = "1.0.1"
+	version     = "1.0.2-dev"
 )
 
 func Description() string {
